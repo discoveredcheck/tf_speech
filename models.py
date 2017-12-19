9708702b67b162dd426d096a3e9c8aa7d000b308
Guillaume de Chambrier <chambrierg@gmail.com>
2017-12-19 11:53:02 +0000

    removed default unused models.

--- models.py
+++ models.py
@@ -103,17 +103,12 @@ def create_model(fingerprint_input, model_settings, model_architecture,
   Raises:
     Exception: If the architecture type isn't recognized.
   """
-  if model_architecture == 'single_fc':
-    return create_single_fc_model(fingerprint_input, model_settings,
-                                  is_training)
-  elif model_architecture == 'conv':
+  if model_architecture == 'conv':
     return create_conv_model(fingerprint_input, model_settings, is_training)
   elif model_architecture == 'conv1d':
     return create_conv1d_model(fingerprint_input, model_settings, is_training)
   elif model_architecture == 'low_latency_conv':
     return create_low_latency_conv_model(fingerprint_input, model_settings,  is_training)
-  elif model_architecture == 'low_latency_svdf':
-    return create_low_latency_svdf_model(fingerprint_input, model_settings, is_training, runtime_settings)
   elif model_architecture == 'attn':
     return create_att_lstm_model(fingerprint_input, model_settings, is_training)
   elif model_architecture == 'lstm':
@@ -149,45 +144,6 @@ def load_variables_from_checkpoint(sess, start_checkpoint):
         print('!!WARNING!! op {} not found in checkpoint'.format(v.name))
     saver = tf.train.Saver(nlist)
     saver.restore(sess, start_checkpoint)
-
-
-def create_single_fc_model(fingerprint_input, model_settings, is_training):
-  """Builds a model with a single hidden fully-connected layer.
-
-  This is a very simple model with just one matmul and bias layer. As you'd
-  expect, it doesn't produce very accurate results, but it is very fast and
-  simple, so it's useful for sanity testing.
-
-  Here's the layout of the graph:
-
-  (fingerprint_input)
-          v
-      [MatMul]<-(weights)
-          v
-      [BiasAdd]<-(bias)
-          v
-
-  Args:
-    fingerprint_input: TensorFlow node that will output audio feature vectors.
-    model_settings: Dictionary of information about the model.
-    is_training: Whether the model is going to be used for training.
-
-  Returns:
-    TensorFlow node outputting logits results, and optionally a dropout
-    placeholder.
-  """
-  if is_training:
-    dropout_prob = tf.placeholder(tf.float32, name='dropout_prob')
-  fingerprint_size = model_settings['fingerprint_size']
-  label_count = model_settings['label_count']
-  weights = tf.Variable(
-      tf.truncated_normal([fingerprint_size, label_count], stddev=0.001))
-  bias = tf.Variable(tf.zeros([label_count]))
-  logits = tf.matmul(fingerprint_input, weights) + bias
-  if is_training:
-    return logits, dropout_prob
-  else:
-    return logits
 
 def create_att_lstm_model(fingerprint_input, model_settings, is_training):
     """
